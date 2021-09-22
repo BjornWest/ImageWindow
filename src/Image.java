85e468375d1e442e7f594ce2def13891f81e475a
BjornWest <70112340+BjornWest@users.noreply.github.com>
2021-09-22 20:55:44 +0200

    Update Image.java

--- src/Image.java
+++ src/Image.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-
-
 public class Image {
     public static double interval = 2000;
 
@@ -16,27 +13,11 @@ public class Image {
         for (int i = 0; i < interval; i++) {
             if (i<interval/2){
                 pointInTime[i][0] = 0.25;
-                pointInTime[i][1] = 0.5;
             }
             else{
                 pointInTime[i][0] = 0.75;
-                pointInTime[i][1] = 0.5;
             }
-
-
-
-
-
-
-
-
-
-
-            /*
- else if (i<*interval/parts){
-                pointInTime[i][1] = -*(i-*interval/parts)/(interval/parts);
-                pointInTime[i][2] = -*(i-*interval/parts)/(interval/parts);
-            } */
+            pointInTime[i][1] = 0.5;
         }
         for (int j = 0; j < interval; j++) {
             pointInTime[j][0] = pointInTime[j][0]*xScale;
@@ -52,14 +33,12 @@ public class Image {
         double[][] pointInTime = information;
         double xTotal;
         double yTotal;
-        int roundx;
-        int roundy;
         for (int a = 0; a< n; a++) {
             xTotal = 0;
             yTotal = 0;
             double nRotate;
             if (a%2==1){
-                nRotate=(a)/2;
+                nRotate=(a+1)/2;
             }
             else {
                 nRotate=-a/2;
@@ -68,19 +47,14 @@ public class Image {
 
                 xTotal = xTotal+rotator.rotation(pointInTime[i][0],pointInTime[i][1],nRotate*(i)/interval*2*Math.PI,xAxis,yAxis)[0]/interval;
                 yTotal = yTotal+rotator.rotation(pointInTime[i][0],pointInTime[i][1],nRotate*(i)/interval*2*Math.PI,xAxis,yAxis)[1]/interval;
-             //   System.out.println(a+" +"+rotator.rotation(pointInTime[i][1],pointInTime[i][2],nRotate*i/interval*2*Math.PI,xAxis,yAxis)[1]/interval);
-             //   System.out.println(a+" "+i+"  "+yTotal);
-             //   System.out.println(a+" "+i+"   "+xTotal);
-
-
             }
 
             integrals[a][0] = xTotal;
             integrals[a][1] = yTotal;
-           // System.out.println(a+":  "+yTotal);
         }
         return integrals;
     }
 
 
 }
+
